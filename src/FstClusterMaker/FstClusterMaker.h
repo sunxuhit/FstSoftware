26b42927f8353f7907708e1a8b1e18281b5c58aa
Xu Sun <sunxuhit@gmail.com>
2020-06-02 21:50:43 -0500

    v3.9: update Scan Clustering Method

--- src/FstClusterMaker/FstClusterMaker.h
+++ src/FstClusterMaker/FstClusterMaker.h
@@ -77,6 +77,7 @@ class FstClusterMaker : public TObject
     std::vector<FstCluster *> findCluster_Simple(std::vector<FstRawHit *> rawHitsVec_orig);
     // cluster with Scan Algorithm
     std::vector<FstCluster *> findCluster_Scan(std::vector<FstRawHit *> rawHitsVec_orig); // only for FST
+    std::vector<FstCluster *> findCluster_ScanRadius(std::vector<FstRawHit *> rawHitsVec_orig); // only for FST
 
     // find track
     std::vector<FstTrack *> findTrack_Hits(std::vector<FstRawHit *> rawHitVec_orig);
