d7aa28b16509f43a67dfea5aa1c6e9990e9a0bff
Xu Sun <sunxuhit@gmail.com>
2021-11-12 18:49:47 -0500

    v2.1: fixed bug for jevp plots

--- FstOnlineZS/plotPedestal.C
+++ FstOnlineZS/plotPedestal.C
@@ -9,7 +9,7 @@
 
 using namespace std;
 
-void plotPedestal(string runId = "01107734")
+void plotPedestal(string runId = "22316056")
 {
   // read in ADC info for all events
   float pedestals[6][3][24][128][9];
