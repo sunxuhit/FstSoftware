6a4ec6ff403c6e3e81bb408b3584a3ddfffd91c0
Xu Sun <sunxuhit@gmail.com>
2022-02-21 15:12:33 -0500

    v3.55: update database

--- FstOffline/macro/database/Calibrations/getPedList.sh
+++ FstOffline/macro/database/Calibrations/getPedList.sh
@@ -6,7 +6,7 @@ cd $PedDirectory
 rm pedRun_run22.list
 rm pedRunNumber_run22.list
 
-ls fst*GOOD.txt > pedRun_run22.list
+ls fst*pedestals_2*GOOD.txt > pedRun_run22.list
 
 awk -F_ '{print $(NF-1)}' pedRun_run22.list | sort | uniq > pedRunNumber_run22.list
 
