548527c0bb9ff937ca427e360dfe30f05f33e3a8
Xu Sun <sunxuhit@gmail.com>
2022-04-18 14:11:06 -0400

    v3.7: complete calibration db of run22

--- FstOffline/macro/database/Calibrations/subDatabase.sh
+++ FstOffline/macro/database/Calibrations/subDatabase.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 inputList=$1
-date="04162022"
+date="04182022"
 
 outputList="/star/u/sunxuhit/ForwardSiliconTracker/Data/FstInstallation/database/inDatabase_${date}.list"
 databaseDir="/star/u/sunxuhit/ForwardSiliconTracker/Data/FstInstallation/database"
